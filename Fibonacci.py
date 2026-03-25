ebe1386bbdd7221358738e1c6c11e8ee9edcbe94
Leonardo <leonardosouzaleite2020@gmail.com>
2026-03-24 22:10:57 -0300

    Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+def Fibonacci(n):
+    # Fibonacci é chato pq precisa de dois casos base. 
+    # Se o índice for 0, o resultado é 0.
+    if(n==0):
+        return 0
+    
+    # Se o índice for 1, o resultado é 1. Sem esses dois, o código se perde.
+    if(n==1):
+        return 1
+    
+    # A lógica da sequência: soma o termo anterior com o retrasado.
+    # É por isso que ela cresce daquele jeito bonito em espiral.
+    return Fibonacci(n-1) + Fibonacci(n-2)
+
+n = 10
+print(f"Fibonacci({n}) = ", Fibonacci(n))
